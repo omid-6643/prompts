405390e3ee27f81f640c24ba94fecf7203b24b5d
omid beigi <o.beygi@gmail.com>
2023-05-31 10:21:55 +0330

    add profile page

---
+++ app/profile/page.jsx
@@ -0,0 +1,37 @@
+"ues client";
+
+import Profile from "@components/Profile";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+const ProfilePage = () => {
+  const { data: session } = useSession();
+
+  const [posts, setPosts] = useState([]);
+
+  const handleEdit = () => {};
+  const handleDelete = async () => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+
+      setPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  return (
+    <Profile
+      name="my"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default ProfilePage;
